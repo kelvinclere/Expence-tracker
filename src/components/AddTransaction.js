7161e1539833aa9c75fb95fbe6cf92151f3c2c20
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-10-07 11:37:59 +0300

    styled the UI

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -24,7 +24,7 @@ export const AddTransaction = () => {
         <h3>Add new transaction</h3>
         <form onSubmit={onSubmit}>
         <div className="form-control">
-            <label htmlfor="text">Text</label>
+            <label htmlfor="text">Description</label>
             <input type="text" value={text} onChange={(e) => setText(e.target.value)}
              placeholder='Enter text...'/>
         </div>
